065a64547b73cd79bb2ef5c422037c18e78bf97c
Won JunSeop <wonjs00@naver.com>
2025-09-07 16:38:27 +0900

    resolve nextjs15 params issue

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,26 +1,24 @@
-import { Suspense } from "react";
-import MovieInfo, { getMovie } from "../../../../components/movie-info";
+import {Suspense} from "react";
+import MovieInfo, {getMovie} from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
-interface IParams {
-    params: { id: string };
-}
-
-export async function generateMetadata({ params: { id } }: IParams) {
+export async function generateMetadata({ params }: { params: Promise<{ id: string }> }) {
+    const {id} = await params;
     const movie = await getMovie(id);
     return {
         title: movie.title,
     };
 }
 
-export default async function MovieDetailPage({ params: { id } }: IParams) {
+export default async function MovieDetailPage({ params }: { params: Promise<{ id: string }> }) {
+    const {id} = await params;
     return (
         <div>
             <Suspense fallback={<h1>Loading movie info</h1>}>
-                <MovieInfo id={id} />
+                <MovieInfo id={id}/>
             </Suspense>
             <Suspense fallback={<h1>Loading movie videos</h1>}>
-                <MovieVideos id={id} />
+                <MovieVideos id={id}/>
             </Suspense>
         </div>
     );
